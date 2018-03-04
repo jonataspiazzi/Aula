5d736a52a44ad0210583959068642d354eb96290
Jonatas Piazzi <piazzi_7@hotmail.com>
2018-03-04 12:07:43 -0300

    Included Exercise 60

---
+++ Exercise60/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] elementos = new int[10];
+            int i;
+
+            //for (i = 0; i < 10; i++)
+            //{
+            //    elementos[i] = int.Parse(Console.ReadLine());
+            //}
+
+            i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine("Digite o elemento " + (i + 1) + ": ");
+                elementos[i] = int.Parse(Console.ReadLine());
+
+                i++;
+            }
+
+            i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine("Elemento " + (i + 1) + ": " + elementos[i]);
+                i++;
+            }
+        }
+    }
+}
