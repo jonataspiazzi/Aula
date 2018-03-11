9b7b355d58f1e5e899723b5264ae98e31ae6e9bc
Jonatas Piazzi <piazzi_7@hotmail.com>
2018-03-11 14:30:11 -0300

    Lesson Matrix

---
+++ CSharp/Aulas/Matriz/v1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Exercise52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            double[,] n;
+            double media;
+            string[] nomes;
+            int qtdAlunos, i;
+
+            Console.Write("Digite a quantidade de alunos: ");
+            qtdAlunos = int.Parse(Console.ReadLine());
+            
+            n = new double[qtdAlunos, 3];
+            nomes = new string[qtdAlunos];
+
+            i = 0;
+            while (i < qtdAlunos)
+            {
+                Console.Write("Digite o nome do aluno " + (i + 1) + ": ");
+                nomes[i] = Console.ReadLine();
+
+                Console.Write("Digite a 1ª nota do aluno " + (i + 1) + ": ");
+                n[i, 0] = double.Parse(Console.ReadLine());
+                
+                Console.Write("Digite a 2ª nota do aluno " + (i + 1) + ": ");
+                n[i, 1] = double.Parse(Console.ReadLine());
+                
+                Console.Write("Digite a 3ª nota do aluno " + (i + 1) + ": ");
+                n[i, 2] = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("-----------");
+                i++;
+            }
+
+            i = 0;
+            while (i < qtdAlunos)
+            {
+                media = (n[i, 0] * 2 + n[i, 1] * 3 + n[i, 2] * 5) / 10;
+                Console.WriteLine("A média do aluno " + nomes[i] + " é " + media);
+                
+                i++;
+            }
+        }
+    }
+}
---
+++ CSharp/Aulas/Matriz/v2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Exercise52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            double[,] notas;
+            double media;
+            string[] nomes;
+            int qtdAlunos, qtdNota, contAluno, contNota;
+
+            qtdNota = 3;
+
+            Console.Write("Digite a quantidade de alunos: ");
+            qtdAlunos = int.Parse(Console.ReadLine());
+            
+            notas = new double[qtdAlunos, qtdNota];
+            nomes = new string[qtdAlunos];
+
+            contAluno = 0;
+            while (contAluno < qtdAlunos)
+            {
+                Console.Write("Digite o nome do aluno " + (contAluno + 1) + ": ");
+                nomes[contAluno] = Console.ReadLine();
+
+                contNota = 0;
+                while (contNota < qtdNota)
+                {
+                    Console.Write("Digite a " + (contNota + 1) + "ª nota do aluno " + (contAluno + 1) + ": ");
+                    notas[contAluno, contNota] = double.Parse(Console.ReadLine());
+
+                    contNota++;
+                }
+
+                Console.WriteLine("-----------");
+                contAluno++;
+            }
+
+            contAluno = 0;
+            while (contAluno < qtdAlunos)
+            {
+                media = (notas[contAluno, 0] * 2 + notas[contAluno, 1] * 3 + notas[contAluno, 2] * 5) / 10;
+                Console.WriteLine("A média do aluno " + nomes[contAluno] + " é " + media);
+                
+                contAluno++;
+            }
+        }
+    }
+}
