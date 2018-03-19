7eb8da92fb2432538012e78ac7f065388ac1ace6
Jonatas Piazzi <piazzi_7@hotmail.com>
2018-03-19 13:52:29 -0300

    Included Exercise 82

---
+++ CSharp/Exercicios/0082/v1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace v1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            int i, qtd, num;
+            int[] v;
+
+            qtd = 5;
+            v = new int[qtd];
+
+            for (i = qtd - 1; i >= 0; i--) // for invertido passa do ultimo ao primeiro.
+            {
+                Console.Write("Digite o " + (i + 1) + "º dígito da esquerda para direita é: ");
+                v[i] = int.Parse(Console.ReadLine());
+            }
+
+            num = 0;
+
+            for (i = 0; i < qtd; i++)
+            {
+                // v[0]*2**4 + v[1]*2**3 + v[2]*2**2
+                num = num + v[i] * (int)Math.Pow(2, i);
+            }
+
+            Console.WriteLine("O valor em decimal é: " + num);
+        }
+    }
+}
