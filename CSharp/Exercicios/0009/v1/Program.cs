2ba88ccbc3bfeeda91023e329ab9786250b27bf8
Jonatas Piazzi <piazzi_7@hotmail.com>
2018-03-26 23:12:08 -0300

    Included Exercise 9 v1

---
+++ CSharp/Exercicios/0009/v1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace v1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont, multiplo;
+
+            cont = 0;
+            while (cont <= 10)
+            {
+                multiplo = 7 * cont;
+                
+                Console.WriteLine("7 x " + cont + " = " + multiplo);
+
+                cont++;
+            }
+        }
+    }
+}
