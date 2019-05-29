44ecdde78d964621a7f61f0f483081407558484f
dextruct0r <dextructor@gmx.com>
2019-05-29 20:10:32 -0300

    Exercise9

---
+++ CSharp/Aulas/Metodos/exercise9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] resultado = new int[11]; 
+
+            Vezes(resultado);
+            for(int i = 0; i <= 10; i++)
+                {
+                    Console.WriteLine($"7X{i}={resultado[i]}");
+                }
+        }
+        static void Vezes (int[] resultado)
+        {
+            for (int qtd = 0; qtd <= 10; qtd++)
+            {
+              resultado[qtd] =  qtd * 7;
+            }            
+        }
+    }
+}
