7dc1780a290cfd69c591bcb1611a40f430d03016
Gênesis Henriques <genesishenriques7@gmail.com>
2019-09-12 20:50:07 -0300

    exercicio 18

---
+++ genesis/exercicios/18/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B ;
+            string texto;
+                       
+            Console.WriteLine("Qual o valor de A?");
+            texto = Console.ReadLine();
+            A = int.Parse(texto);
+            Console.WriteLine("Qual o valor de B?");
+            texto = Console.ReadLine();
+            B = int.Parse(texto);
+            if (A >= B)
+            {
+                Console.WriteLine(A + "é maior ou igual a" + B);
+            }
+            else 
+            {
+                Console.WriteLine(A + "é menor do que " + B);
+            }
+
+        }
+    }
+}
