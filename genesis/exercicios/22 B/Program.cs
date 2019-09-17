cd39294a199a3865c80faf81cfb6b7ee1725a2bd
Gênesis Henriques <genesishenriques7@gmail.com>
2019-09-17 20:09:04 -0300

    exercicio 22 e 22B

---
+++ genesis/exercicios/22 B/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _22_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            string texto;
+          
+            num = 0;
+
+            Console.WriteLine("Digite o numero: ");
+            texto = Console.ReadLine();
+            num = int.Parse(texto);
+           
+            if ( num%2==0 )
+            {
+           num = num * 3;
+            }
+            else
+            {
+            num = num * 2;
+            }
+
+            Console.WriteLine( num );
+        }
+    }
+}
---
+++ genesis/exercicios/22/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, impar, par;
+            string texto;
+          
+            num = 0;
+            impar = 0;
+            par = 0;
+
+            Console.WriteLine("Digite o numero: ");
+            texto = Console.ReadLine();
+            num = int.Parse(texto);
+           
+            if ( num%2==0 )
+            {
+            impar = num * 3;
+            Console.WriteLine( impar );
+            }
+            else
+            {
+            par = num * 2;
+            Console.WriteLine( par );
+            }
+        }
+    }
+}
