ae9ae12935feeb75400f55dfb7352134b5445fe8
Gênesis Henriques <genesishenriques7@gmail.com>
2019-09-24 21:59:48 -0300

    exercicio 28

---
+++ genesis/exercicios/27/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           decimal salario, vendas, valor, total, comissao;
+           string texto;
+
+           Console.WriteLine(" Qual o valor do salario?");
+           texto = Console.ReadLine();
+           salario = int.Parse(texto);
+
+           Console.WriteLine(" Qual o valor total em vendas?");
+           texto = Console.ReadLine();
+           vendas = int.Parse(texto);
+
+           Console.WriteLine("Qual a porcentagem da comissão?");
+           texto = Console.ReadLine();
+           valor = int.Parse(texto);
+
+           comissao = valor / 100M * vendas;
+
+           total = salario + comissao;
+        
+            Console.WriteLine("O valor a receber em comissão é: " + comissao );
+            Console.WriteLine("O valor total a receber é: " + total);
+        }
+    }
+}
---
+++ genesis/exercicios/28/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal num, a, b, c;
+            string texto;
+
+            Console.WriteLine("Operações que iremos fazer");
+            Console.WriteLine("A - O resto da divisão de X / 5");
+            Console.WriteLine("B - X² + 20");
+            Console.WriteLine("C - X² - X + 2");
+
+            Console.WriteLine("Digite o valor de X");
+            texto = Console.ReadLine();
+            num = int.Parse(texto);
+
+            a = num % 5;
+
+            b = num * num + 20;
+
+            c = num * num - num + 2;
+
+            Console.WriteLine("O valor de A é: " + a);
+            Console.WriteLine("O valor de B é: " + b);
+            Console.WriteLine("O valor de C é: " + c);
+            
+
+        }
+    }
+}
