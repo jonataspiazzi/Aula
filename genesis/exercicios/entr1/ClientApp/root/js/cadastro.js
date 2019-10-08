fac06b88264fea471ea64d6ae370717d102cc61a
Jônatas Piazzi <piazzi_7@hotmail.com>
2019-10-08 20:31:50 -0300

    Configurou layout da tela de cadastro

--- genesis/exercicios/entr1/ClientApp/root/js/cadastro.js
+++ genesis/exercicios/entr1/ClientApp/root/js/cadastro.js
@@ -5,39 +5,6 @@ document.addEventListener("DOMContentLoaded", function() {
   listarFuncionariosDoServidor();
 });
 
-function novoFuncionario() {
-  var corpoTabela = document.querySelector('#funcionarios tbody');
-  var linha = document.createElement("tr");
-
-  corpoTabela.appendChild(linha);
-
-  var c1 = document.createElement("td");
-  c1.innerHTML = '<input type="checkbox" />';
-  linha.appendChild(c1);
-
-  var c2 = document.createElement("td");
-  c2.innerHTML = "Karina Vasconcelos";
-  linha.appendChild(c2);
-
-  var c3 = document.createElement("td");
-  c3.innerHTML = "Customer Success";
-  linha.appendChild(c3);
-
-  var c4 = document.createElement("td");
-  c4.innerHTML = "a@iclips.com.br";
-  linha.appendChild(c4);
-
-  var c5 = document.createElement("td");
-  c5.innerHTML = "(32) 1020-3040";
-  linha.appendChild(c5);
-
-  var c6 = document.createElement("td");
-  linha.appendChild(c6);
-
-  var c7 = document.createElement("td");
-  linha.appendChild(c7);
-}
-
 function listarFuncionariosDoServidor() {
     fetch('/api/funcionarios')
         .then((res) => {
@@ -77,9 +44,18 @@ function adicionaFuncionarioNaTabela(funcionario) {
     linha.appendChild(c5);
 
     var c6 = document.createElement("td");
+    c6.style.textAlign = 'center';
+    c6.innerHTML = '<input type="checkbox" class="input-toggle" checked disabled />';
     linha.appendChild(c6);
 
     var c7 = document.createElement("td");
+    c7.innerHTML = `
+      <div class="grid-button">
+        <img src="/images/visualizar.png" />
+        <img src="/images/editar.png" />
+        <img src="/images/excluir.png" />
+      </div>
+    `;
     linha.appendChild(c7);
 }
 
