e3683f8d0c369276e247eceea42131c100805338
Jônatas Piazzi <piazzi_7@hotmail.com>
2019-10-09 21:22:46 -0300

    Editando funcionario na tabela

--- genesis/exercicios/entr1/ClientApp/root/js/cadastro.js
+++ genesis/exercicios/entr1/ClientApp/root/js/cadastro.js
@@ -25,46 +25,58 @@ function listarFuncionariosDoServidor() {
 }
 
 function adicionaFuncionarioNaTabela(funcionario) {
-    var corpoTabela = document.querySelector('#funcionarios tbody');
-    var linha = document.createElement("tr");
-    linha.id = funcionario.idFuncionario;
+  var corpoTabela = document.querySelector('#funcionarios tbody');
+  var linha = document.createElement("tr");
+  linha.id = funcionario.idFuncionario;
 
-    corpoTabela.appendChild(linha);
+  corpoTabela.appendChild(linha);
 
-    var c1 = document.createElement("td");
-    c1.innerHTML = '<input type="checkbox" />';
-    linha.appendChild(c1);
+  var c1 = document.createElement("td");
+  c1.innerHTML = '<input type="checkbox" />';
+  linha.appendChild(c1);
 
-    var c2 = document.createElement("td");
-    c2.innerHTML = funcionario.nome;
-    linha.appendChild(c2);
+  var c2 = document.createElement("td");
+  c2.innerHTML = funcionario.nome;
+  linha.appendChild(c2);
 
-    var c3 = document.createElement("td");
-    c3.innerHTML = funcionario.departamento;
-    linha.appendChild(c3);
+  var c3 = document.createElement("td");
+  c3.innerHTML = funcionario.departamento;
+  linha.appendChild(c3);
 
-    var c4 = document.createElement("td");
-    c4.innerHTML = funcionario.email;
-    linha.appendChild(c4);
+  var c4 = document.createElement("td");
+  c4.innerHTML = funcionario.email;
+  linha.appendChild(c4);
 
-    var c5 = document.createElement("td");
-    c5.innerHTML = funcionario.telefonePrimario;
-    linha.appendChild(c5);
+  var c5 = document.createElement("td");
+  c5.innerHTML = funcionario.telefonePrimario;
+  linha.appendChild(c5);
 
-    var c6 = document.createElement("td");
-    c6.style.textAlign = 'center';
-    c6.innerHTML = `<input type="checkbox" class="input-toggle" ${funcionario.acessoAoIClips ? 'checked' : ''} disabled />`;
-    linha.appendChild(c6);
+  var c6 = document.createElement("td");
+  c6.style.textAlign = 'center';
+  c6.innerHTML = `<input type="checkbox" class="input-toggle" ${funcionario.acessoAoIClips ? 'checked' : ''} disabled />`;
+  linha.appendChild(c6);
 
-    var c7 = document.createElement("td");
-    c7.innerHTML = `
-      <div class="grid-button">
-        <img src="/images/visualizar.png" onclick="visualizar(${funcionario.idFuncionario})" />
-        <img src="/images/editar.png" onclick="editar(${funcionario.idFuncionario})" />
-        <img src="/images/excluir.png" onclick="excluir(${funcionario.idFuncionario})" />
-      </div>
-    `;
-    linha.appendChild(c7);
+  var c7 = document.createElement("td");
+  c7.innerHTML = `
+    <div class="grid-button">
+      <img src="/images/visualizar.png" onclick="visualizar(${funcionario.idFuncionario})" />
+      <img src="/images/editar.png" onclick="editar(${funcionario.idFuncionario})" />
+      <img src="/images/excluir.png" onclick="excluir(${funcionario.idFuncionario})" />
+    </div>
+  `;
+  linha.appendChild(c7);
+}
+
+function editaFuncionarioNaTabela(funcionario) {
+  const linha = document.querySelector(`#funcionarios tbody tr[id=${funcionario.idFuncionario}]`);
+
+  if (!linha) return;
+
+  linha.querySelector('td:nth-child(2)').innerHTML = funcionario.nome;
+  linha.querySelector('td:nth-child(3)').innerHTML = funcionario.departamento;
+  linha.querySelector('td:nth-child(4)').innerHTML = funcionario.email;
+  linha.querySelector('td:nth-child(5)').innerHTML = funcionario.telefonePrimario;
+  linha.querySelector('td:nth-child(6) input[type=checkbox]').checked = funcionario.telefonePrimario;
 }
 
 function novo() {
@@ -133,6 +145,11 @@ function salvar() {
       funcionarios.push(fun);
   
       adicionaFuncionarioNaTabela(fun);
+    } else {
+      const index = funcionarios.indexOf(funcionario);
+      funcionarios.splice(index, 1, fun);
+
+      editaFuncionarioNaTabela(fun);
     }
   
     fecharModal();
