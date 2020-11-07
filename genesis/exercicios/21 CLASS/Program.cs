f816903be82479de584ac62bdf90acd06d126ae8
Genesis <genesishenriques7@gmail.com>
2020-11-07 10:59:06 -0300

    exercicios 21 e 22 feitos com classe

---
+++ genesis/exercicios/21 CLASS/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _21_CLASS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantas pessoas participaram da pesquisa?");
+            var max = int.Parse(Console.ReadLine());
+
+            Pesquisa P = new Pesquisa();
+
+            P.idade = new int[max];  P.nome = new string[max];
+
+            for (var i = 0; i < max; i++)
+            {
+                Console.WriteLine($"Qual o nome da {1+i}º pessoa?");
+                P.nome[i] = Console.ReadLine();
+
+                Console.WriteLine("E qual a sua idade?");
+                P.idade[i] = int.Parse(Console.ReadLine());
+
+                if (P.idade[i] > 20)
+                {
+                    P.maiorQueVinte = P.maiorQueVinte + P.nome[i] + "-";
+
+                    if (P.idade[i] > 40)
+                    {
+                        P.maiorQueQuarenta++;
+                    }
+                }
+                
+            }
+
+            Console.WriteLine($"{P.maiorQueVinte} tem mais de vinte anos.");
+            Console.WriteLine($"E {P.maiorQueQuarenta} pessoas tem mais de quarenta anos.");
+
+        }
+    }
+    class Pesquisa
+    {
+        public string maiorQueVinte;
+        public int maiorQueQuarenta;
+        public string [] nome;
+        public int [] idade;
+    }
+}
---
+++ genesis/exercicios/22 CLASS/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _22_CLASS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numero N = new Numero();
+            
+            for(var i = 1; i > 0; i++)
+            {
+                Console.WriteLine("Digite um numero ou digite 0 para terminar");
+                N.num = int.Parse(Console.ReadLine());
+
+                if(N.num == 0)
+                {
+                    break;
+                }
+                else if (N.num % 2 == 0) // par
+                {
+                    N.GetPar(N.num);
+                }
+                else if (N.num % 2 == 1)
+                {
+                    N.GetImpar(N.num);
+                }
+                
+            }
+        }
+    }
+    class Numero
+    {
+        public int num;
+        public void GetImpar(int num)
+        {
+            var vezesTres = num * 3;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"Um numero IMPAR!!");
+            Console.WriteLine($"Um numero IMPAR!! O valor de {num} multiplicado por 3 é: {vezesTres}");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        public void GetPar(int num)
+        {
+            var vezesDois = num * 2;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"Um numero PAR!!");
+            Console.WriteLine($"O valor de {num} multiplicado por 2 é: {vezesDois}");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
