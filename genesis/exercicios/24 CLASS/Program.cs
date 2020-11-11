45aca7e1867df532c4c2a807c0b76e320dde9ea6
GenesisHenriques <genesishenriques7@gmail.com>
2020-11-10 22:50:34 -0300

    Exercicio 24-Class

---
+++ genesis/exercicios/24 CLASS/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _24_CLASS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValorAbsoluto V = new ValorAbsoluto();
+
+            Console.WriteLine("Digite um numero");
+            V.num = int.Parse(Console.ReadLine());
+
+            //aqui e...
+            var absoluto = V.GetDescobrirValor(V.num);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"O valor absoluto de {V.num} é: {absoluto}!!");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            
+          
+        }
+    }
+    class  ValorAbsoluto
+    {
+        public int num;
+        public int GetDescobrirValor(int num)
+        {
+            //... aqui eu usei o mesmo nome de variavel, analisando o codigo, o que eu fiz foi algo "errado" no contexto do padrão dos programadores? 
+            var absoluto = num;
+            return num;
+        }
+    }
+}
